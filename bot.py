bcec855ec6d2bcde0747962ea3404e4876a521f0
jenabictak <jenabi1991@gmail.com>
2024-11-26 15:12:41 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# دستور /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"سلام {update.effective_user.first_name}! این ربات Railway هست.")
+
+if __name__ == "__main__":
+    # توکن ربات تلگرام
+    TOKEN = "8187523450:AAGE1Ard4no0HPZdBdl6kitl41vld-I62PM"
+    
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    
+    print("ربات شروع به کار کرد...")
+    app.run_polling()
